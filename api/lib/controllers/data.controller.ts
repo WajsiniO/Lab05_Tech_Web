31b448c1ff9beb44dc7c3bf1053134a54ac7aaba
WajsiniO <alekswajs@interia.pl>
2024-03-22 14:23:18 +0100

    Własna realizacja
    działa

--- api/lib/controllers/data.controller.ts
+++ api/lib/controllers/data.controller.ts
@@ -15,7 +15,18 @@ class DataController implements Controller {
     private initializeRoutes() {
         this.router.get(`${this.path}/latest`,
         this.getLatestReadingsFromAllDevices);
+        
         this.router.post(`${this.path}/:id`, this.addData);
+        
+        this.router.get(`${this.path}/:id`, this.getData);
+
+        this.router.get(`${this.path}/:id/latest`, this.getLatestData);
+
+        this.router.get(`${this.path}/:id/:num`, this.getRange);
+
+        this.router.delete(`${this.path}/all`, this.deleteAll);
+
+        this.router.delete(`${this.path}/:id`, this.deleteElem);
     }
 
     private getLatestReadingsFromAllDevices = async (request: Request, response: Response, next: NextFunction) => {
@@ -30,6 +41,44 @@ class DataController implements Controller {
 
         response.status(200).json(`Dodano ${id}`);
     }
+
+    private getData = async (request: Request, response: Response, next: NextFunction) => {
+        const {id} = request.params;
+        const data = testArr;
+
+        response.status(200).json(data[Number(id)]);
+    }
+
+    private getLatestData = async (request: Request, response: Response, next: NextFunction) => {
+        const {id} = request.params;
+        const data = testArr;
+        const max = data.reduce((a, b) => Math.max(a, b), -Infinity);
+
+        response.status(200).json(max);
+
+    }
+
+    private getRange = async (request: Request, response: Response, next: NextFunction) => {
+        const {id} = request.params;
+        const {num} = request.params;
+        const data = testArr;
+
+        response.status(200).json(data.splice(Number(id), Number(num)));
+    }
+
+    private deleteAll = async (request: Request, response: Response, next: NextFunction) => {
+        testArr = [];
+        response.status(200).json(`Wyczyszczono tablice`);
+    }
+
+    private deleteElem = async (request: Request, response: Response, next: NextFunction) => {
+        const {id} = request.params;
+
+        testArr.splice(Number(id), 1)
+        
+        response.status(200).json(`Usunięto element o indeksie ${id}`);
+    }
+
 }
 
 export default DataController;
